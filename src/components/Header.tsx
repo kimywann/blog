0214f2efe768fa9c7f142e66ecbefb3512cd8116
kimywann <kimywan10@gmail.com>
2025-09-23 22:25:01 +0900

    헤더 컴포넌트 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex items-center justify-between">
+        <Link
+          href="/"
+          className="font-medium font-mono cursor-pointer text-gray-400"
+        >
+          Kimyngwan
+        </Link>
+
+        <div className="flex items-center space-x-6">
+          <Link
+            href="https://imminent-lunch-7f5.notion.site/Frontend-Developer-1e06975159448067af00fad1fba789b7?source=copy_link"
+            className="font-mono cursor-pointer text-pink-500"
+          >
+            resume
+          </Link>
+          <Link
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-mono cursor-pointer text-pink-500"
+          >
+            github
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}

ef155bfab99142063454a11497ea82c86a85af19
kimywann <kimywan10@gmail.com>
2025-10-01 16:27:13 +0900

    refactor: 외부 링크를 푸터로 재배치

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,30 +3,13 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <nav className="flex items-center justify-between">
+      <nav className="flex items-center">
         <Link
           href="/"
           className="font-medium text-2xl cursor-pointer text-gray-400"
         >
-          Kimyngwan
+          김영완 블로그
         </Link>
-
-        <div className="flex items-center space-x-6">
-          <Link
-            href="https://imminent-lunch-7f5.notion.site/Frontend-Developer-1e06975159448067af00fad1fba789b7?source=copy_link"
-            className="cursor-pointer text-blue-500 font-medium"
-          >
-            resume
-          </Link>
-          <Link
-            href="https://github.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="cursor-pointer text-blue-500 font-medium"
-          >
-            github
-          </Link>
-        </div>
       </nav>
     </header>
   );
